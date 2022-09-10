76e19bea50ea289260b379f0bd838a4eb00addc9
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-09-09 22:51:55 -0300

    Feat: Create Slider with keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/future/image'
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import tShirt1 from '../assets/t-shirt-1.png'
 import tShirt2 from '../assets/t-shirt-2.png'
+import tShirt3 from '../assets/t-shirt-3.png'
 
+import 'keen-slider/keen-slider.min.css'
 import { HomeContainer, Product } from '../styles/pages/home'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tShirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -16,11 +27,27 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={tShirt2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tShirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tShirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Z</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
